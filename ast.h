f4ef14d14533578096400aaa33610b538a43dd97
Dan Setterquist <dan.setterquist@bontouch.com>
2018-11-14 19:52:12 +0100

    Add differentiation.

--- ast.h
+++ ast.h
@@ -5,133 +5,150 @@
 #include <map>
 
 using Variables = std::map<std::string, double>;
+using Dual = std::pair<double, double>;
 
 class Node {
-public:
+ public:
   virtual ~Node() = default;
-  virtual double value(const Variables& vars) const = 0;
+  virtual Dual value(const Variables& vars, const std::string& d) const = 0;
 };
 
 class Scalar: public Node {
-private:
+ private:
   double scalar;
-public:
-  Scalar(double scalar): scalar(scalar) {}
+ public:
+ Scalar(double scalar): scalar(scalar) {}
 
-  double value(const Variables& vars) const override {
-    return scalar;
+  Dual value(const Variables& vars, const std::string& d) const override {
+    return std::make_pair(scalar, 0.0);
   }
 };
 
 class Variable: public Node {
-private:
+ private:
   std::string name;
-public:
-  Variable(std::string name): name(name) {}
+ public:
+ Variable(std::string name): name(name) {}
 
-  double value(const Variables& vars) const override {
-    return vars.at(name);
+  Dual value(const Variables& vars, const std::string& d) const override {
+    double e = name == d ? 1.0 : 0.0;
+    return std::make_pair(vars.at(name), e);
   }
 };
 
 class BinaryOp: public Node {
-protected:
+ protected:
   std::unique_ptr<Node> left;
   std::unique_ptr<Node> right;
-public:
-  BinaryOp(std::unique_ptr<Node> left, std::unique_ptr<Node> right)
-    : left(std::move(left)), right(std::move(right)) {}
+ public:
+ BinaryOp(std::unique_ptr<Node> left, std::unique_ptr<Node> right)
+   : left(std::move(left)), right(std::move(right)) {}
 };
 
 class Add: public BinaryOp {
-public:
-  Add(std::unique_ptr<Node> left, std::unique_ptr<Node> right)
-    : BinaryOp(std::move(left), std::move(right)) {}
+ public:
+ Add(std::unique_ptr<Node> left, std::unique_ptr<Node> right)
+   : BinaryOp(std::move(left), std::move(right)) {}
 
-  double value(const Variables& vars) const override {
-    return left->value(vars) + right->value(vars);
+  Dual value(const Variables& vars, const std::string& d) const override {
+    Dual l = left->value(vars, d);
+    Dual r = right->value(vars, d);
+    return std::make_pair(l.first + r.first, l.second + r.second);
   }
 };
 
 class Sub: public BinaryOp {
-public:
-  Sub(std::unique_ptr<Node> left, std::unique_ptr<Node> right)
-    : BinaryOp(std::move(left), std::move(right)) {}
+ public:
+ Sub(std::unique_ptr<Node> left, std::unique_ptr<Node> right)
+   : BinaryOp(std::move(left), std::move(right)) {}
 
-  double value(const Variables& vars) const override {
-    return left->value(vars) - right->value(vars);
+  Dual value(const Variables& vars, const std::string& d) const override {
+    Dual l = left->value(vars, d);
+    Dual r = right->value(vars, d);
+    return std::make_pair(l.first - r.first, l.second - r.second);
   }
 };
 
 class Mul: public BinaryOp {
-public:
-  Mul(std::unique_ptr<Node> left, std::unique_ptr<Node> right)
-    : BinaryOp(std::move(left), std::move(right)) {}
+ public:
+ Mul(std::unique_ptr<Node> left, std::unique_ptr<Node> right)
+   : BinaryOp(std::move(left), std::move(right)) {}
 
-  double value(const Variables& vars) const override {
-    return left->value(vars) * right->value(vars);
+  Dual value(const Variables& vars, const std::string& d) const override {
+    Dual l = left->value(vars, d);
+    Dual r = right->value(vars, d);
+    return std::make_pair(l.first * r.first, l.second * r.first + l.first * r.second);
   }
 };
 
 class Div: public BinaryOp {
-public:
-  Div(std::unique_ptr<Node> left, std::unique_ptr<Node> right)
-    : BinaryOp(std::move(left), std::move(right)) {}
+ public:
+ Div(std::unique_ptr<Node> left, std::unique_ptr<Node> right)
+   : BinaryOp(std::move(left), std::move(right)) {}
 
-  double value(const Variables& vars) const override {
-    return left->value(vars) / right->value(vars);
+  Dual value(const Variables& vars, const std::string& d) const override {
+    Dual l = left->value(vars, d);
+    Dual r = right->value(vars, d);
+    return std::make_pair(l.first / r.first, (l.second * r.first - l.first * r.second) / (r.first * r.first));
   }
 };
 
-class Pow: public BinaryOp {
-public:
-  Pow(std::unique_ptr<Node> left, std::unique_ptr<Node> right)
-    : BinaryOp(std::move(left), std::move(right)) {}
+class Pow: public Node {
+ private:
+  std::unique_ptr<Node> child;
+  double scalar;
+ public:
+ Pow(std::unique_ptr<Node> child, double scalar): child(std::move(child)), scalar(scalar) {}
 
-  double value(const Variables& vars) const override {
-    return std::pow(left->value(vars), right->value(vars));
+  Dual value(const Variables& vars, const std::string& d) const override {
+    Dual c = child->value(vars, d);
+    return std::make_pair(std::pow(c.first, scalar), scalar * std::pow(c.first, scalar - 1) * c.second);
   }
 };
 
 class UnaryOp: public Node {
-protected:
+ protected:
   std::unique_ptr<Node> child;
-public:
-  UnaryOp(std::unique_ptr<Node> child): child(std::move(child)) {}
+ public:
+ UnaryOp(std::unique_ptr<Node> child): child(std::move(child)) {}
 };
 
 class Sin: public UnaryOp {
-public:
-  Sin(std::unique_ptr<Node> child): UnaryOp(std::move(child)) {}
+ public:
+ Sin(std::unique_ptr<Node> child): UnaryOp(std::move(child)) {}
 
-  double value(const Variables& vars) const override {
-    return std::sin(child->value(vars));
+  Dual value(const Variables& vars, const std::string& d) const override {
+    Dual c = child->value(vars, d);
+    return std::make_pair(std::sin(c.first), c.second * std::cos(c.first));
   }
 };
 
 class Cos: public UnaryOp {
-public:
-  Cos(std::unique_ptr<Node> child): UnaryOp(std::move(child)) {}
+ public:
+ Cos(std::unique_ptr<Node> child): UnaryOp(std::move(child)) {}
 
-  double value(const Variables& vars) const override {
-    return std::cos(child->value(vars));
+  Dual value(const Variables& vars, const std::string& d) const override {
+    Dual c = child->value(vars, d);
+    return std::make_pair(std::cos(c.first), - c.second * std::sin(c.first));
   }
 };
 
 class Exp: public UnaryOp {
-public:
-  Exp(std::unique_ptr<Node> child): UnaryOp(std::move(child)) {}
+ public:
+ Exp(std::unique_ptr<Node> child): UnaryOp(std::move(child)) {}
 
-  double value(const Variables& vars) const override {
-    return std::exp(child->value(vars));
+  Dual value(const Variables& vars, const std::string& d) const override {
+    Dual c = child->value(vars, d);
+    return std::make_pair(std::exp(c.first), c.second * std::exp(c.first));
   }
 };
 
 class NatLog: public UnaryOp {
-public:
-  NatLog(std::unique_ptr<Node> child): UnaryOp(std::move(child)) {}
+ public:
+ NatLog(std::unique_ptr<Node> child): UnaryOp(std::move(child)) {}
 
-  double value(const Variables& vars) const override {
-    return std::log(child->value(vars));
+  Dual value(const Variables& vars, const std::string& d) const override {
+    Dual c = child->value(vars, d);
+    return std::make_pair(std::log(c.first), c.second / c.first);
   }
 };
--- parseresult.h
+++ parseresult.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "ast.h"
+#include <set>
 #include <map>
 
 struct ParseResult {
   std::unique_ptr<Node> ast;
+  std::set<std::string> arguments;
   std::map<std::string, double> vars;
 };
